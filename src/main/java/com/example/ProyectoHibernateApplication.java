abf0df84ff0b7665d4afa7aa131ac8dacd3b7153
Jose Antonio <j2a2mari@gmail.com>
2021-04-12 00:25:31 +0200

    fist commit

---
+++ src/main/java/com/example/ProyectoHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoHibernateApplication.class, args);
+	}
+
+}
